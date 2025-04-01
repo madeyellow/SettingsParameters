62e605325953dcde68f2febc5ef2bb185d09c9ec
buharizin <buharizinevgeniy@gmail.com>
2025-04-01 22:13:09 +0600

    Added new StringSettingsParameter implementation

---
+++ Runtime/StringSettingsParameter.cs
@@ -0,0 +1,26 @@
+﻿using MadeYellow.SettingsParameters.Abstractions;
+using UnityEngine;
+
+namespace MadeYellow.SettingsParameters
+{
+    /// <summary>
+    /// Implements a <see cref="string"/> parameter. Think of it as of slider
+    /// </summary>
+    [System.Serializable]
+    public class StringSettingsParameter : SettingsParameterBase<string>
+    {
+        public StringSettingsParameter(string prefrencesKey, string defaultValue = null) : base(prefrencesKey, defaultValue)
+        {
+        }
+
+        protected override string ReadValue(string defaultValue)
+        {
+            return PlayerPrefs.GetString(Key, defaultValue);
+        }
+
+        protected override void WriteValue()
+        {
+            PlayerPrefs.SetString(Key, Value);
+        }
+    }
+}
